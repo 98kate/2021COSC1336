d0cc5784e56353f8193ac1dc1cd09bbbecf14620
98kate <84046696+98kate@users.noreply.github.com>
2021-05-11 17:24:13 -0500

    Add files via upload

---
+++ hw1-businesscard/businesscard.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Fujiya Japanese Garden" << std::endl;
+    std::cout << "\n";
+    std::cout << "9030 Wurzbach Rd, San Antonio, TX 78240" << std::endl;
+    std::cout << "\n";
+    std::cout << "(210)615-7553" << std::endl;
+    std::cout << "\n";
+    std::cout << "fujiyajapanesegarden.com" << std::endl;
+    std::cout << "\n";
+
+    return 0;
+}
